da959ab41f82dd760bcc60d55b869e4ad7a21686
Jihun Yun <yunjyhoon@cau.ac.kr>
2023-06-16 22:56:37 +0900

    7.success

---
+++ 7.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define SizeX 100
+#define SizeY 20
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// #include <windows.h>
+
+struct mych
+{
+    char ch;
+    int pos;
+    int speed;
+};
+
+void resetScreen(char (*in)[SizeX][SizeY])
+{
+    for (int i = 0; i < SizeY; i++)
+    {
+        for (int j = 0; j < SizeX; j++)
+        {
+            (*in)[j][i] = ' ';
+        }
+    }
+}
+
+void printScreen(char (*in)[SizeX][SizeY])
+{
+    for (int i = 0; i < SizeY; i++)
+    {
+        for (int j = 0; j < SizeX; j++)
+        {
+            printf("%c", (*in)[j][i]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    struct mych listCh[26];
+    char screen[SizeX][SizeY];
+    char(*ptScreen)[SizeX][SizeY] = &screen;
+    resetScreen(ptScreen);
+
+    for (int i = 0; i < 26; i++)
+    {
+        int tempVal = rand() % (SizeX * SizeY);
+        listCh[i].pos = tempVal;
+        listCh[i].speed = rand() % 10 + 1;
+        listCh[i].ch = i + 'A';
+
+        screen[tempVal % SizeX][tempVal / SizeX] = i + 'A';
+    }
+
+    int i = 0;
+
+    while (1)
+    {
+        printScreen(ptScreen);
+        resetScreen(ptScreen);
+        printf("%d\n", i);
+        Sleep(1000);
+        i++;
+        // system("cls");
+
+        for (int i = 0; i < 26; i++)
+        {
+            listCh[i].pos += listCh[i].speed;
+            int tempVal = listCh[i].pos % (SizeX * SizeY);
+            listCh[i].pos = tempVal;
+            listCh[i].ch = i + 'A';
+
+            screen[tempVal % SizeX][tempVal / SizeX] = i + 'A';
+        }
+    }
+}
